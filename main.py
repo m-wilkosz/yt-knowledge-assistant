fac36f7d74787cef7f289a789163bc0722aaef51
m-wilkosz <wilkosz1998@gmail.com>
2023-09-04 08:42:37 +0200

    timestamps

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def seconds_to_hms(seconds):
     hours = int(seconds) // 3600
     minutes = (int(seconds) % 3600) // 60
     seconds = int(seconds) % 60
-    return f'{hours:02}:{minutes:02}:{seconds:02}'
+    return f'[{hours:02}:{minutes:02}:{seconds:02}]' \
+            f'(https://www.youtube.com/watch?v=tIeHLnjs5U8&t={hours}h{minutes}m{seconds}s)'
 
 def create_timestamps_string(seconds: Set[str]) -> str:
     if not seconds:
